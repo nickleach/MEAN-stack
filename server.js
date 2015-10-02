86dc2d674a51c3755682b2afe8d057443e939b2d
Nick Leach <nickleach22@gmail.com>
2015-10-01 23:07:49 -0400

    Added mongodb and mongoose

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+ // Require some stuff
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+ // Declare express
 var app = express();
 
+ // Compile stylus
 function compile(str, path){
   return stylus(str).set('filename', path);
 }
@@ -17,7 +23,6 @@ app.use(logger('dev'));
 app.use(bodyParser());
 app.use(stylus.middleware(
   {
-
     src: __dirname + '/public',
     compile: compile
   }
@@ -26,17 +31,41 @@ app.use(stylus.middleware(
 app.get('/partials/:partialPath', function(req, res){
   res.render('partials/' + req.params.partialPath);
 });
+
 app.use(express.static(__dirname + '/public'));
+
+
+// declare mongoose
+mongoose.connect('mongodb://localhost/mean');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error....'));
+db.once('open', function callback(){
+  console.log('mean db opened');
+});
+
+
+
+// make a schema
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+  mongoMessage = messageDoc.message;
+});
+
 
 app.get('*', function(req, res){
 
-  res.render('index');
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 
 });
 
+// make port
 var port = 3030;
 
 app.listen(port);
-console.log("Listening on port " + port);
+console.log("Listening on port " + port + "...");
 
 
